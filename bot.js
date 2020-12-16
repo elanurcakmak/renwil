5561025ac1aa76656b51d17890298f3f41d3bc5c
elanurcakmak <72741369+elanurcakmak@users.noreply.github.com>
2020-12-16 14:12:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,33 @@
-const Discord = require("discord.js");//Lord Creative
-const client = new Discord.Client();//Lord Creative
-const ayarlar = require("./ayarlar.json");//Lord Creative
-const chalk = require("chalk");//Lord Creative
-const moment = require("moment");//Lord Creative
-var Jimp = require("jimp");//Lord Creative
-const { Client, Util } = require("discord.js");//Lord Creative
-const weather = require("weather-js");//Lord Creative
-const fs = require("fs");//Lord Creative
-const db = require("quick.db");//Lord Creative
-const http = require("http");//Lord Creative
-const express = require("express");//Lord Creative
-require("./util/eventLoader.js")(client);//Lord Creative
-const path = require("path");//Lord Creative
-const request = require("request");//Lord Creative
-const snekfetch = require("snekfetch");//Lord Creative
-const queue = new Map();//Lord Creative
-const YouTube = require("simple-youtube-api");//Lord Creative
-const ytdl = require("ytdl-core");//Lord Creative
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const db = require('wio.db');
+const http = require('http');
+const express = require('express');
+const moment = require("moment");
+const app = express();
+require("moment-duration-format");
+app.get("/", (request, response) => {
+console.log("ArdaDemr | Hostlandı");
+response.sendStatus(200);
+});
+app.listen(8000);
+setInterval(() => {
+http.get(`http://ozelfyukas.glitch.me/`);
+}, 280000)
+require('./util/eventLoader')(client);
 
-const app = express();//Lord Creative
-app.get("/", (request, response) => {//Lord Creative
-  console.log(Date.now() + "Lord Creative  7/24 AKTİF TUTMA İŞLEMİ BAŞARILI");//Lord Creative
-  response.sendStatus(200);//Lord Creative
-});//Lord Creative
-app.listen(process.env.PORT);//Lord Creative
-setInterval(() => {//Lord Creative
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);//Lord Creative
-}, 280000);//Lord Creative
-//Lord Creative
-var prefix = ayarlar.prefix;//Lord Creative
+var prefix = ayarlar.prefix;
 
-const log = message => {//Lord Creative
-  console.log(`${message}`);//Lord Creative
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
 
-client.commands = new Discord.Collection();//Lord Creative
+client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir("./komutlar/", (err, files) => {
+fs.readdir('./komutlar/', (err, files) => {
   if (err) console.error(err);
   log(`${files.length} komut yüklenecek.`);
   files.forEach(f => {
@@ -46,7 +37,7 @@ fs.readdir("./komutlar/", (err, files) => {
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
     });
-  });//Lord Creative
+  });
 });
 
 client.reload = command => {
@@ -63,9 +54,9 @@ client.reload = command => {
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
-    } catch (e) {
+    } catch (e){
       reject(e);
-    }//Lord Creative
+    }
   });
 };
 
@@ -78,14 +69,14 @@ client.load = command => {
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
-    } catch (e) {
-      reject(e);//Lord Creative
+    } catch (e){
+      reject(e);
     }
   });
 };
 
 client.unload = command => {
-  return new Promise((resolve, reject) => {//Lord Creative
+  return new Promise((resolve, reject) => {
     try {
       delete require.cache[require.resolve(`./komutlar/${command}`)];
       let cmd = require(`./komutlar/${command}`);
@@ -94,78 +85,75 @@ client.unload = command => {
         if (cmd === command) client.aliases.delete(alias);
       });
       resolve();
-    } catch (e) {
+    } catch (e){
       reject(e);
-    }//Lord Creative
+    }
   });
 };
 
-client.elevation = message => {
-  if (!message.guild) {
-    return;
-  }
-  let permlvl = 0;
-  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-  if (message.author.id === ayarlar.sahip) permlvl = 4;
-  return permlvl;
-};
 
 var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-// client.on('debug', e => {//Lord Creative
+// client.on('debug', e => {
 //   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
 // });
 
-client.on("warn", e => {
-  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
 });
 
-client.on("error", e => {//Lord Creative
-  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token)
-/////
+client.login(ayarlar.token);
 
-//---------------------------------KOMUTLAR---------------------------------\\
-//Lord Creative
-///otorol///
-client.on("guildMemberAdd", async (member, guild, message) => {
-  let role = db.fetch(`otorolisim_${member.guild.id}`);
-  let otorol = db.fetch(`autoRole_${member.guild.id}`);//Lord Creative
-  let i = db.fetch(`otorolKanal_${member.guild.id}`);
-  if (!otorol || otorol.toLowerCase() === "yok") return;
-  else {
-    try {
-      if (!i) return;
-      if (!role) {
-        member.addRole(member.guild.roles.get(otorol));
-        var embed = new Discord.RichEmbed()
-          .setDescription(
-            "**Sunucuya Yeni Katılan** @" +
-              member.user.tag +
-              " **Kullanıcısına** <@&" +
-              otorol +
-              ">  **Rolü verildi:white_check_mark:**"
-          )
-          .setColor("0x36393E")
-          .setFooter(`wonders Otorol Sistemi`);
-        member.guild.channels.get(i).send(embed);
-      } else if (role) {
-        member.addRole(member.guild.roles.get(otorol));
-        var embed = new Discord.RichEmbed()
-          .setDescription(
-            `**Sunucuya Yeni Katılan** \`${member.user.tag}\` **Kullanıcısına** \`${role}\` **Rolü verildi. <aHg Karşim :D 696373472431177781>**`
-          )
-          .setColor("0x36393E")
-          .setFooter(`Fays Otorol Sistemi`);
-        member.guild.channels.get(i).send(embed);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-});
+//---------------------------------komutlar---------------------------------\\
+client.on("guildMemberAdd", async member => {
+if (!member.user.bot) return;
+await member.guild.ban(member.guild.member(member))
+})
+
+//---------------------------------DDOS KORUMASI-----------------------------\\
+client.on('message', msg => {
+
+if(client.ping > 2500) {
+
+            let bölgeler = ['singapore', 'eu-central', 'india', 'us-central', 'london',
+            'eu-west', 'amsterdam', 'brazil', 'us-west', 'hongkong', 
+            'us-south', 'southafrica', 'us-east', 'sydney', 'frankfurt',
+            'russia']
+           let yenibölge = bölgeler[Math.floor(Math.random() * bölgeler.length)]
+           let sChannel = msg.guild.channels.find(c => c.name === "ddos-system")
+
+           sChannel.send(`Sunucu'ya Vuruyorlar \nSunucu Bölgesini Değiştirdim \n __**${yenibölge}**__ :tik: __**Sunucu Pingimiz**__ :`+ client.ping)
+           msg.guild.setRegion(yenibölge)
+           .then(g => console.log(" bölge:" + g.region))
+           .then(g => msg.channel.send("bölge **"+ g.region  + " olarak değişti")) 
+           .catch(console.error);
+}});
+//---------------------------------DDOS KORUMASI-----------------------------\\
+
+client.on("roleDelete", async(role , channel , message , guild) => {
+  let rolkoruma = await db.fetch(`rolk_${role.guild.id}`);
+    if (rolkoruma == "acik") {
+  role.guild.createRole({name: role.name, color: role.color,  permissions: role.permissions}) 
+        role.guild.owner.send(` **${role.name}** Adlı Rol Silindi Ve Ben Rolü Tekrar Oluşturdum  :white_check_mark::`)
+
+  
+}
+})  
+
+//--------------------KANAL KORUMA--------------------------------\\
+client.on("channelDelete", async channel => {
+  const logs = await channel.guild.fetchAuditLogs({ type: 'CHANNEL_DELETE' }).then(audit => audit.entries.first())
+  const deleter = await channel.guild.members.get(logs.executor.id);
+  if(deleter.id == "") return; //bu satıra kendi id'nizi yazın sizin kanal silmenizi engellemeyecektir
+  channel.clone(undefined, true, true, "Kanal silme koruması sistemi").then(async klon => {
+    await klon.setParent(channel.parent);
+    await klon.setPosition(channel.position);
+  })
+})
+
 ///küfür///
 client.on("message", async msg => {
   if (msg.author.bot) return;
@@ -177,6 +165,7 @@ client.on("message", async msg => {
       "amcık",
       "yarrak",
       "orospu",
+      "oç",
       "piç",
       "sikerim",
       "sikik",
@@ -204,7 +193,7 @@ client.on("message", async msg => {
           msg.delete();
           let embed = new Discord.RichEmbed()
             .setColor(0xffa300)
-            .setFooter("PinkCOde Küfür Sistemi", client.user.avatarURL)
+            .setFooter("Küfür Sistemi", client.user.avatarURL)
             .setAuthor(
               msg.guild.owner.user.username,
               msg.guild.owner.user.avatarURL
@@ -235,181 +224,4 @@ client.on("message", async msg => {
   }
   if (!i) return;
 });
-///reklam///
-client.on("message", async msg => {
-  let antoxd = await db.fetch(`antoxd${msg.guild.id}`);
-  if (antoxd === "acik") {//Lord Creative
-    const reklam = ["discord.gg", "https://discordapp.com/invite/"];
-    if (reklam.some(word => msg.content.includes(word))) {
-      msg.delete();
-    }
-  }
-});
-///sayaç///
-client.on("guildMemberAdd", async member => {
-  let frenzysayı = await db.fetch(`FrenzyCode+SayaçSayı_${member.guild.id}`);
-  let frenzykanal = await db.fetch(`FrenzyCode+SayaçKanal_${member.guild.id}`);
-  if (!frenzysayı || !frenzykanal) return;
-  let sonuç = frenzysayı - member.guild.memberCount;
-  client.channels
-    .get(frenzykanal)
-    .send(
-      `<a:blobjoining:698617723684519967> | O Sunucumuza Yeni Biri Geldi Ve İsmi ${member}, Hoşgeldin  **${frenzysayı}** Kişiye Ulaşmak İçin  **${sonuç}** Kişi Kaldı.`
-    );
-});
-client.on("guildMemberRemove", async member => {
-  let frenzysayı = await db.fetch(`FrenzyCode+SayaçSayı_${member.guild.id}`);
-  let frenzykanal = await db.fetch(`FrenzyCode+SayaçKanal_${member.guild.id}`);
-  if (!frenzysayı || !frenzykanal) return;
-  let sonuç = frenzysayı - member.guild.memberCount;
 
-  client.channels
-    .get(frenzykanal)
-    .send(
-      `<a:ablobleaving:698617725936861214> | Olamaz ${member}, Sunucudan Ayrıldı! **${frenzysayı}** Kişiye Ulaşmak İçin  **${sonuç}** Kişi Kaldı.`
-    );
-  return;
-});
-///sa-as///
-client.on("message", async msg => {
-
-
-  const i = await db.fetch(`ssaass_${msg.guild.id}`);
-    if (i == 'acik') {
-      if (msg.content.toLowerCase() == 'sa' || msg.content.toLowerCase() == 's.a' || msg.content.toLowerCase() == 'selamun aleyküm') {
-          try {
-
-                  return msg.reply('Aleyküm Selam, Hoşgeldin')
-          } catch(err) {
-            console.log(err);
-          }
-      }
-    }
-    else if (i == 'kapali') {
-    
-    }
-    if (!i) return;
-
-    });
-
-///reklam-engelle///
-client.on("message", async msg => {
-  if (msg.author.bot) return;
-  if (msg.channel.type === "dm") return;
-
-  let i = await db.fetch(`reklamFiltre_${msg.guild.id}`);
-  if (i == "acik") {
-    const reklam = [
-      "discord.app",
-      "discord.gg",
-      "invite",
-      "discordapp",
-      "discordgg",
-      ".com",
-      ".net",//Lord Creative
-      ".xyz",
-      ".tk",
-      ".pw",
-      ".io",
-      ".me",
-      ".gg",
-      "www.",
-      "https",
-      "http",
-      ".gl",
-      ".org",
-      ".com.tr",
-      ".biz",
-      ".party",
-      ".rf.gd",
-      ".az"
-    ];
-    if (reklam.some(word => msg.content.toLowerCase().includes(word))) {
-      try {
-        if (!msg.member.hasPermission("MANAGE_GUILD")) {
-          msg.delete();
-          let embed = new Discord.RichEmbed()
-            .setColor(0xffa300)
-            .setFooter(
-              "Renwil BOT  -|-  Reklam engellendi.",
-              client.user.avatarURL
-            )
-            .setAuthor(
-              msg.guild.owner.user.username,
-              msg.guild.owner.user.avatarURL
-            )
-            .setDescription(
-              " Renwil Reklam Sistemi, " +
-                `**${msg.guild.name}**` +
-                " Adlı Sunucuda Reklam Yakaladım."
-            )
-            .addField(
-              "Reklamı yapan kişi",
-              "Kullanıcı: " + msg.author.tag + "\nID: " + msg.author.id,
-              true
-            )
-            .addField("Engellenen mesaj", msg.content, true)
-            .setTimestamp();
-          msg.guild.owner.user.send(embed);
-          return msg.channel
-            .send(`${msg.author.tag}, Reklam Yapmak Yasak!`)
-            .then(msg => msg.delete(25000));
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  }
-  if (!i) return;
-});
-///davet-ayarla///
-const invites = {};
-
-const wait = require('util').promisify(setTimeout);
-
-client.on('ready', () => {
-
-  wait(1000);
-
-  client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
-    });
-  });
-});
-
-client.on('guildMemberAdd', member => {
-  
-  
- 
-  member.guild.fetchInvites().then(guildInvites => {
-    
-    if (db.has(`dKanal_${member.guild.id}`) === false) return
-    const channel = db.fetch(`dKanal_${member.guild.id}`).replace("<#", "").replace(">", "")
-    
-    const ei = invites[member.guild.id];
-  
-    invites[member.guild.id] = guildInvites;
- 
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-
-    const davetçi = client.users.get(invite.inviter.id);
-     db.add(`davet_${invite.inviter.id + member.guild.id}`,1)
-let bal  = db.fetch(`davet_${invite.inviter.id + member.guild.id}`)
-   member.guild.channels.get(channel).send(`<a:blobjoining:696373472431177781> ** <@${member.id}> Joined**; İnvited by **${davetçi.tag}** (`+'**'+bal+'** invites)')
-  })
-
-});
-client.on("guildMemberRemove", async member => {
-//Lord Creative
-    member.guild.fetchInvites().then(guildInvites => {
-
-      const ei = invites[member.guild.id];
-  
-    invites[member.guild.id] = guildInvites;
- 
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-
-       db.subtract(`davet_${invite.inviter.id + member.guild.id}`,1)
-    })
-})
