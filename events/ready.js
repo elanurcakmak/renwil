b770867d25bab9a6aadfc4c7ca60e676b59054c7
elanurcakmak <72741369+elanurcakmak@users.noreply.github.com>
2020-12-16 14:22:25 +0300

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,28 @@
 const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const ayarlar = require('../ayarlar.json');
-const dateFormat = require('dateformat');
 
+var prefix = ayarlar.prefix;
 
 module.exports = client => {
- client.user.setActivity('0', {type: "PLAYİNG"})
- client.user.setGame(client.guilds.size + ' Sunucu-' + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ' kullanıcıya hizmet veriliyor.');
-  console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+var oyun = [
+        "-yardım - Otorol Güncellendi🔥",
+        "-yardım - Küfür Engel✨",
+        "-yardım - Reklam Engel⭐️"  
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "https://www.youtube.com/channel/UCHUp3vJlRN98vdLU1oVX9VQ" );
+        }, 2 * 2500);
+    
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setActivity(`${prefix}yardım ` + client.guilds.size + ` Sunucu ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` Kullanıcı `);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
